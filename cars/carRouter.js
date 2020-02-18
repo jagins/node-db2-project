0df58249faa956a4c1f6979d178705921cde7284
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-18 15:51:21 -0600

    get cars by id works and tested in postman. error 404 statement works too

--- cars/carRouter.js
+++ cars/carRouter.js
@@ -16,4 +16,24 @@ router.get('/', (req, res) =>
     })
 })
 
+router.get('/:id', (req, res) =>
+{
+    database('car-dealer').where({id: req.params.id}).first()
+    .then(car =>
+    {
+        if(car)
+        {
+            res.status(200).json(car);
+        }
+        else
+        {
+            res.status(404).json({message: 'The Car with that ID does not exist'});
+        }
+    })
+    .catch(error =>
+    {
+        res.status(500).json({error: `Couldn't retrieve Cars from the database`});
+    })
+})
+
 module.exports = router;
