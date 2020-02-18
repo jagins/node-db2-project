4d863d021ce16462285eb3c238024ce2b1a06b3d
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-18 15:40:50 -0600

    get all cars works. tested in postman displays all 6 cars that were set as seeds

--- cars/carRouter.js
+++ cars/carRouter.js
@@ -5,7 +5,15 @@ const router = express.Router();
 
 router.get('/', (req, res) =>
 {
-   
+   database('car-dealer')
+   .then(cars =>
+    {
+        res.status(200).json(cars);
+    })
+   .catch(error =>
+    {
+        res.status(500).json({error: `Couldn't retrieve Cars from the database`});
+    })
 })
 
 module.exports = router;
