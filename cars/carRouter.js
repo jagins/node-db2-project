db6688708d2fa23a17ccd4657971650dad11a052
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-18 16:26:02 -0600

    add new car works and returns the car object instead of the the newly created id

--- cars/carRouter.js
+++ cars/carRouter.js
@@ -36,4 +36,49 @@ router.get('/:id', (req, res) =>
     })
 })
 
+router.post('/', validateCar, (req, res) =>
+{
+    const addCar = {
+        VIN: req.body.VIN,
+        MAKE: req.body.MAKE,
+        MODEL: req.body.MODEL,
+        MILEAGE: req.body.MILEAGE,
+        TRANSMISSION: req.body.TRANSMISSION ? req.body.TRANSMISSION : null,
+        STATUS: req.body.STATUS ? req.body.STATUS : null
+    };
+
+    database('car-dealer').insert(addCar, 'id')
+    .then(newId =>
+    {
+        database('car-dealer').where({id: newId[0]}).first()
+        .then(car =>
+        {
+            res.status(201).json(car);
+        })
+        .catch(error =>
+        {
+            res.status(500).json({error: 'Could not retrieve Cars from the database'});
+        })
+    })
+    .catch(error =>
+    {
+        res.status(500).json({error: 'Error saving new Car to the database'});
+    })
+})
+
+function validateCar(req, res, next)
+{
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0)
+    {
+      res.status(400).json({message: 'missing car data'});
+    }
+    else if(!req.body.VIN || !req.body.MAKE || !req.body.MODEL || !req.body.MILEAGE)
+    {
+        res.status(400).json({message: 'VIN Number, MAKE, MODEL, and MILEAGE are required'});
+    }
+    else
+    {
+        next();
+    }
+}
 module.exports = router;
